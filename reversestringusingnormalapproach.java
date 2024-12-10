bb6a9d6e0a577bc3e9f8195c534c52ee6b927b80
A Janani Priya <janu47537@gmail.com>
2024-12-10 12:05:35 +0530

    Add files via upload

---
+++ reversestringusingnormalapproach.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class reversestringusingnormalapproach{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        String res=" ";
+        for(int i=0;i<a.length();i++)
+        {
+            res=a.charAt(i)+res;
+            
+        }
+        System.out.println(res);
+    }
+}
