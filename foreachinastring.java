7e41269c0b78262d78811d5cf5b23f8aa413b8bc
A Janani Priya <janu47537@gmail.com>
2024-12-10 12:24:42 +0530

    Add files via upload

---
+++ foreachinastring.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class foreachinastring{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        String a[]={"Apple","Orange","Pineapple"};
+        for(String b:a)
+        System.out.print(b+",");
+    }
+
+}
